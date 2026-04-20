30634fe818fb1473ac86f79d0032e318e149a644
AYUSH KUMAR <ayushdham405@gmail.com>
2026-04-20 21:43:18 +0530

    Add Local Server Script

---
+++ serve.ps1
@@ -0,0 +1,40 @@
+$port = 8081
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:$port/")
+$listener.Start()
+Write-Host "Server started at http://localhost:$port/"
+try {
+    while ($listener.IsListening) {
+        $context = $listener.GetContext()
+        $request = $context.Request
+        $response = $context.Response
+        
+        $path = $request.Url.LocalPath
+        if ($path -eq "/") { $path = "/insta.html" }
+        $filePath = Join-Path (Get-Location) $path.Replace('/', '\').TrimStart('\')
+        
+        if (Test-Path $filePath -PathType Leaf) {
+            $extension = [System.IO.Path]::GetExtension($filePath)
+            $contentType = switch ($extension) {
+                ".html" { "text/html" }
+                ".css"  { "text/css" }
+                ".js"   { "application/javascript" }
+                ".png"  { "image/png" }
+                ".jpg"  { "image/jpeg" }
+                default { "application/octet-stream" }
+            }
+            
+            $content = [System.IO.File]::ReadAllBytes($filePath)
+            $response.ContentType = $contentType
+            $response.ContentLength64 = $content.Length
+            $response.OutputStream.Write($content, 0, $content.Length)
+        } else {
+            $response.StatusCode = 404
+            $errorMsg = [System.Text.Encoding]::UTF8.GetBytes("404 - Not Found")
+            $response.OutputStream.Write($errorMsg, 0, $errorMsg.Length)
+        }
+        $response.Close()
+    }
+} finally {
+    $listener.Stop()
+}
